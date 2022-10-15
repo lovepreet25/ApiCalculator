00777aea94492ef53ece88ecce249d02c61bb7f2
Lovepreet <klovepreet121999@gmail.com>
2022-10-15 14:58:46 -0400

    changesSertting up the API Controllers

---
+++ Controllers/CalculatorApi.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCalculator.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CalculatorApi : ControllerBase
+    {
+        [HttpGet]
+        // method for addition
+        public int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        [HttpGet]
+        //method for subtracttionn
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        [HttpGet]
+        //method multiplication of the numbers
+        public int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        //method fot dividing the numbers
+        [HttpGet]
+        public int dividation(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
+
+
+    
