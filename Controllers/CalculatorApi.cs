9b97f635b47e4c68fff707b1c81261ff3c071a11
Lovepreet <klovepreet121999@gmail.com>
2022-10-15 14:58:46 -0400

    Add project files.

---
+++ Controllers/CalculatorApi.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCalculator.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CalculatorApi : ControllerBase
+    {
+        [HttpGet]
+        public int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        [HttpGet]
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        [HttpGet]
+        public int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        [HttpGet]
+        public int dividation(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
+
+namespace CalcaulationsLibrary
+{
+    public class calculation
+    {
+        public int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        public int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        public int division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
